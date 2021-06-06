b3ee09cc46110a580785afca82b6f32ffc434db4
pecrisandrade <pecrisandrade@gmail.com>
2021-06-06 19:54:07 -0300

    Inicialização do uso do localStorage

--- livraria_da_gente/src/store/index.js
+++ livraria_da_gente/src/store/index.js
@@ -2,11 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: "",
+    users: JSON.parse(localStorage.getItem('users')) ?? [],
   },
   mutations: {
+    // setUsername(state, val) {
+    //   state.user = val
+    // },
+    // setPassword(state, val){
+    //   state.password = val
+    //   localStorage.setItem('users', JSON.stringify(state.users))
+    // },
+    addUser(state, user) {
+         state.users.push(user);
+         console.log(state.users);
+         localStorage.setItem('users', JSON.stringify(state.users));
+    },
   },
   actions: {
   },
   modules: {
-  }
+  },
 })
