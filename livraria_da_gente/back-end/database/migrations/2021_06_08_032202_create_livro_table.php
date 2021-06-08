1e1299f7f8ac4cd84c50c24fb3daf6385ae364e5
alinevenceslau <alineepecris79@gmail.com>
2021-06-08 00:55:56 -0300

    criando tabela livro

---
+++ livraria_da_gente/back-end/database/migrations/2021_06_08_032202_create_livro_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLivroTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('livro', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('genero');
+            $table->string('subtitulo');
+            $table->integer('edicao');
+            $table->string('isbn');
+            $table->string('estado');
+            $table->integer('valor');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('livro');
+    }
+}
