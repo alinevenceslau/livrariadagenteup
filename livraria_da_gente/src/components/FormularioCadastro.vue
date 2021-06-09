7077528a78d9d3bf777dedc6ab893d1499f5126e
Marcelo Antonio <marceloa.mnzs@gmail.com>
2021-06-08 22:14:49 -0300

    Estilização da tela de cadastro #11

--- livraria_da_gente/src/components/FormularioCadastro.vue
+++ livraria_da_gente/src/components/FormularioCadastro.vue
@@ -1,13 +1,24 @@
 <template>
-
-<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 min-w-sm">
-    <h1>Cadastro de Usuário</h1><br>
-    Nome: <input type="text" placeholder="Nome de Usuário" class="border-none text-black mr-3 focus:outline-none"><br>
-    Email: <input type="text" placeholder="E-mail" class="border-none text-black mr-3 focus:outline-none">  <br>
-    Senha: <input type="text" placeholder="Senha" class="border-none text-black mr-3 focus:outline-none"><br>
-    Confirma: <input type="text" placeholder="Confirme sua Senha" class="border-none text-black mr-3 focus:outline-none"><br>
-    <button class="bg-blue-500 hover:bg-blue-700
-         text-white font-bold py-1 px-4 rounded-full">Cadastrar</button>
+    <form> 
+        <div class="flex items-center justify-center h-screen">
+            <div class="bg-gray-100 shadow-2xl rounded mb-20 max-w-5xl h-4/6 w-2/6 ">
+                <h1 class="py-5 text-3xl font-bold text-center">Cadastro de Usuário</h1>
+                <div class="grid ml-10 ">
+                    <label class="labelForm">Nome:</label>
+                    <input type="text" placeholder="Nome de Usuário" class="border-none text-black focus:outline-none ml-3 w-80">
+                
+                    <label class="labelForm">E-mail:</label>
+                    <input type="text" placeholder="E-mail" class="border-none text-black focus:outline-none ml-3 w-80">
+                
+                    <label class="labelForm">Senha:</label>
+                    <input type="text" placeholder="Senha" class="border-none text-black focus:outline-none ml-3  w-80">
+                
+                    <label class="labelForm">Confime a sua senha:</label>
+                    <input type="text" placeholder="Confirme sua Senha" class="border-none text-black focus:outline-none ml-3 w-80">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full my-5 justify-self-center">Cadastrar</button>
+                </div>
+            </div>
+        </div>
     </form>
 </template>
 
