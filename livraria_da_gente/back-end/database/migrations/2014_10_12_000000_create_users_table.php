79e006cc7f262520d84ec1a25a8461f4a879fecb
alinevenceslau <alineepecris79@gmail.com>
2021-06-12 01:19:51 -0300

    GH-12 adicionando valor default

--- livraria_da_gente/back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ livraria_da_gente/back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('saldo');
+            $table->integer('saldo')->default('0');
             $table->rememberToken();
             $table->timestamps();
         });
