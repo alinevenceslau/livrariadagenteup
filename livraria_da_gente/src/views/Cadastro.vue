bdc09576ea3786e7a76778092906068f0082093e
Marcelo Antonio <marceloa.mnzs@gmail.com>
2021-07-06 04:07:09 -0300

    Criação das telas de login e cadastro #10

--- livraria_da_gente/src/views/Cadastro.vue
+++ livraria_da_gente/src/views/Cadastro.vue
@@ -1,18 +1,64 @@
 <template>
-    <div class="justify-align-center">
-        <Cadastro/>
+    <FormHeader/>
+    <div class="object-center w-full max-w-xs mx-auto">
+        <form class="shadow-xl p-8">
+
+            <div class="flex flex-wrap -mx-3 mb-4">
+                <!-- Input e label de senha -->
+                <div class="w-full px-3">
+                    <label class="block tracking-wide text-gray-700 font-bold mb-2">
+                        Nome:
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200     rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"   type="text" placeholder="Digite o seu nome" name="nome" required>
+                </div>
+            </div>
+
+            <div class="flex flex-wrap -mx-3 mb-4">
+                <!-- Input e label de e-mail -->
+                <div class="w-full px-3">
+                    <label class="block tracking-wide text-gray-700 font-bold mb-2">
+                        E-mail:
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="ex: livrariadagente@email.com" name="email" required>
+                </div>
+            </div>
+
+
+            <div class="flex flex-wrap -mx-3 mb-4">
+                <!-- Input e label de senha -->
+                <div class="w-full px-3">
+                    <label class="block tracking-wide text-gray-700 font-bold mb-2">
+                        Senha:
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200     rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"   type="password" placeholder="********" name="senha" required>
+                </div>
+            </div>
+
+            <div class="flex flex-wrap -mx-3 mb-4">
+                <!-- Input e label de Repetição senha -->
+                <div class="w-full px-3">
+                    <label class="block tracking-wide text-gray-700 font-bold mb-2">
+                        Confirme sua Senha:
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200     rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"   type="password" placeholder="********" name="confirma" required>
+                </div>
+                
+                <button class="mx-auto bg-blue-900 hover:bg-blue text-white font-base hover:text-white py-2 mt-6 px-16 border border-blue hover:border-transparent rounded">
+                    Cadastrar
+                </button>
+            </div>
+        </form>
     </div>
+    
 </template>
 
 <script>
-import Cadastro from "../components/FormularioCadastro.vue"
-export default {
-    components:{
+    import FormHeader from '@/components/FormHeader.vue'
+    export default {
+        name: 'Cadastro',
 
-        Cadastro
+        components:{
+            FormHeader
+        }
     }
-}
 </script>
-<style scoped>
-
-</style>
--- livraria_da_gente/src/views/Login.vue
+++ livraria_da_gente/src/views/Login.vue
@@ -1,18 +1,42 @@
 <template>
-<div class="justify-align-center">
-<Formulario />
-  </div>
+    <FormHeader/>
+    <div class="object-center w-full max-w-xs mx-auto">
+        <form class="shadow-xl p-8">
+            <div class="flex flex-wrap -mx-3 mb-4">
+                <!-- Input e label de e-mail -->
+                <div class="w-full px-3">
+                    <label class="block tracking-wide text-gray-700 font-bold mb-2">
+                        E-mail:
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="ex: livrariadagente@email.com" name="email" required>
+                </div>
+            </div>
+
+            <div class="flex flex-wrap -mx-3 mb-4">
+                <!-- Input e label de senha -->
+                <div class="w-full px-3">
+                    <label class="block tracking-wide text-gray-700 font-bold mb-2">
+                        Senha:
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200     rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"   type="password" placeholder="********" name="senha" required>
+                </div>
+                
+                <button class="mx-auto bg-blue-900 hover:bg-blue text-white font-base hover:text-white py-2 mt-6 px-16 border border-blue hover:border-transparent rounded">
+                    Entrar
+                </button>
+            </div>
+        </form>
+    </div>
+    
 </template>
 
 <script>
-import Formulario from "@/components/FormularioLogin.vue"
-export default {
-  components: {
-    Formulario
-  }
-}
+    import FormHeader from '@/components/FormHeader.vue'
+    export default {
+        name: 'Login',
+
+        components:{
+            FormHeader
+        }
+    }
 </script>
-
-<style scoped>
-
-</style>
