d4842d51b6c900df15fc4988f8d3dc2434a2031b
Marcelo Antonio <marceloa.mnzs@gmail.com>
2021-08-04 20:32:44 -0300

    Estilizando página de meu acervo

--- livraria_da_gente/src/views/MeuAcervo.vue
+++ livraria_da_gente/src/views/MeuAcervo.vue
@@ -1,11 +1,31 @@
 <template>
     <Navbar/>
 
-    <div class="flex items-center justify-center min-w-screen">
-        <div class="grid grid-cols-3 gap-2">
+    <h1 class="text-center text-2xl mb-5">Meu acervo</h1>
+
+    <div class="flex items-center justify-center min-w-screen px-10">
+        <div class="grid grid-cols-4 gap-1">
             <div v-for="livro in livros" :key="livro.id">
-                <div class="bg-blue-100 px-36 rounded h-96">
-                    {{livro.titulo}} 
+                <div class="border px-5 py-5 rounded h-116 flex flex-col">
+                    
+                    <!-- Imagem do livro -->
+                    <div class="bg-gray-200 h-3/5 flex justify-center items-center">
+                        <span class="text-center font-bold">Imagem do livro aqui</span>
+                    </div>
+                    
+                    <div class="flex-grow">
+                        <!-- Título -->
+                        <div class="pt-3 flex flex-col">
+                            <span class="text-lg font-medium">{{livro.titulo}} - {{livro.subtitulo}}</span>
+                            <span class="py-2">{{livro.autor}}</span>
+                        </div>
+                    </div>
+
+                    <!-- Botões -->
+                    <div class="pb-1">
+                        <button href="#" class="py-2 px-6 float-left border border-blue-900 text-blue-900 rounded hover:bg-blue-900 hover:text-white">Editar livro</button>
+                        <button href="#" class="py-2 px-6 float-right bg-red-500 hover:bg-red-700 text-white rounded">Deletar livro</button>
+                    </div>
                 </div>
             </div>      
         </div>
