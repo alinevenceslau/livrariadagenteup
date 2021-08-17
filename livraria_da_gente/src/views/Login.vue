dc2adb253b38b9803e182d58c340f82bc4383532
Marcelo Antonio <marceloa.mnzs@gmail.com>
2021-08-17 17:18:51 -0300

    Estilizando view de Login

--- livraria_da_gente/src/views/Login.vue
+++ livraria_da_gente/src/views/Login.vue
@@ -1,32 +1,37 @@
 <template>
+    <div class="flex justify-center items-center flex-col py-28">
     <FormHeader/>
-    <div class="object-center w-full max-w-xs mx-auto">
-        <form class="shadow-xl p-8" @submit.prevent="login">
-            <div class="flex flex-wrap -mx-3 mb-4">
-                <!-- Input e label de e-mail -->
-                <div class="w-full px-3">
-                    <label class="block tracking-wide text-gray-700 font-bold mb-2">
-                        E-mail:
-                    </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="ex: livrariadagente@email.com" name="email" v-model="email" required>
+        <div class="max-w-sm">
+            <form class="py-4 mx-auto" @submit.prevent="login">
+                <div class="flex flex-wrap -mx-3 mb-4">
+                    <!-- Input e label de e-mail -->
+                    <div class="w-full px-3">
+                        <label class="block tracking-wide text-gray-700 font-bold mb-2">
+                            E-mail:
+                        </label>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="ex: livrariadagente@email.com" name="email" v-model="email" required>
+                    </div>
                 </div>
-            </div>
 
-            <div class="flex flex-wrap -mx-3 mb-4">
-                <!-- Input e label de senha -->
-                <div class="w-full px-3">
-                    <label class="block tracking-wide text-gray-700 font-bold mb-2">
-                        Senha:
-                    </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200     rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"   type="password" placeholder="********" name="senha" v-model="password" required>
+                <div class="flex flex-wrap -mx-3 mb-4">
+                    <!-- Input e label de senha -->
+                    <div class="w-full px-3">
+                        <label class="block tracking-wide text-gray-700 font-bold mb-2">
+                            Senha:
+                        </label>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200     rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"   type="password" placeholder="********" name="senha" v-model="password" required>
+                    </div>
+
+                    <button class="mx-auto bg-blue-900 hover:bg-blue text-white font-base hover:text-white py-2 mt-6 px-16 border border-blue hover:border-transparent rounded">
+                        Entrar
+                    </button>
                 </div>
-                
-                <button class="mx-auto bg-blue-900 hover:bg-blue text-white font-base hover:text-white py-2 mt-6 px-16 border border-blue hover:border-transparent rounded">
-                    Entrar
-                </button>
-            </div>
-        </form>
+            </form>
+            <p>Não possui uma conta? Clique aqui para se <a class="font-bold cursor-pointer" @click.prevent="redirectToSignUp">Cadastrar</a></p>
+            <router-view/>
+        </div>
     </div>
+    
     
 </template>
 
@@ -54,6 +59,10 @@
                     this.$router.push('/')
                 }
             },
+
+            redirectToSignUp(){
+                this.$router.push('/cadastro')
+            }
         }
     }
 </script>
